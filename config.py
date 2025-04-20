f29c59baeed5cd2d6a53569c2f5183a425864f11
bensonnlee <bensonlee0524@gmail.com>
2025-04-20 00:10:46 -0700

    heroku - option to log to stdout

--- config.py
+++ config.py
@@ -19,3 +19,5 @@ class Config:
     LANGUAGES = ['en', 'es']
     MS_TRANSLATOR_KEY = os.environ.get('MS_TRANSLATOR_KEY')
     POSTS_PER_PAGE = 25
+
+    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
